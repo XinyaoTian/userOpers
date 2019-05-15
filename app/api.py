f9587ee513e5e4b011e5bb74c7558565c2440269
leon <xinyaotian@XinyaoTians-MacBook-Pro.local>
2019-05-15 14:31:10 +0800

    validate password and create new user

--- app/api.py
+++ app/api.py
@@ -1,36 +1,75 @@
-from app import app
+from app import app, db
 from app.models import User
 from flask import jsonify
+from flask import request, url_for
+from werkzeug.http import HTTP_STATUS_CODES
 
 
 # get user by user id
 @app.route('/users/uid/<int:uid>', methods=['GET'])
 def get_user_by_uid(uid):
-    return jsonify(User.query.get_or_404(uid).to_dict())
+    data = list()
+    data.append(User.query.get_or_404(uid).to_dict())
+    return jsonify(data)
 
 
 # get user by username
 @app.route('/users/username/<string:username>', methods=['GET'])
 def get_user_by_username(username):
-    return jsonify(User.query.filter(User.username == username).first().to_dict())
+    data = list()
+    data.append(User.query.filter(User.username == username).first_or_404().to_dict())
+    return jsonify(data)
 
 
 # get user by phone_number
 @app.route('/users/phone_number/<phone_number>', methods=['GET'])
 def get_user_by_phone_number(phone_number):
-    pass
+    data = list()
+    data.append(User.query.filter(User.phone_number == phone_number).first_or_404().to_dict())
+    return jsonify(data)
 
 
 # get all users
 @app.route('/users', methods=['GET'])
 def get_all_users():
-    pass
+    data = list()
+    for user in User.query.all():
+        data.append(user.to_dict())
+    return jsonify(data)
 
 
 # create new user
 @app.route('/users', methods=['POST'])
 def create_new_user():
-    pass
+    username = request.form.get('username')
+    password = request.form.get('password')
+    email = request.form.get('email')
+    phone_number = request.form.get('phone_number')
+
+    # check faults
+    if password is None or username is None:
+        return bad_request('This post must include both username and password field.')
+    if email is None or phone_number is None:
+        return bad_request('This post must include both email and phone_number field.')
+    if User.query.filter_by(username=username).first():
+        return bad_request('please use a different username.')
+    if User.query.filter_by(username=email).first():
+        return bad_request('please use a different email address.')
+    if User.query.filter_by(username=phone_number).first():
+        return bad_request('please use a different phone number.')
+
+    # db operations
+    new_user = User(username=username, email=email, phone_number=phone_number)
+    new_user.set_password(password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    # response
+    response = jsonify(new_user.to_dict())
+    response.status_code = 201
+    response.headers['Location'] = url_for('get_user_by_uid', uid=new_user.id)
+
+    return response
 
 
 # update user info
@@ -39,6 +78,37 @@ def update_user():
     pass
 
 
+# verify username and password
+@app.route('/users/validation/', methods=['POST'])
+def validate_password():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # if there is no password field in post
+    if password is None or username is None:
+        return bad_request('This post must include both username and password field.')
+    user = User.query.filter(User.username == username).first()
+    if user is None:
+        return jsonify({'username': username, 'validation': 'False'})
+    validate = user.check_password(password)
+
+    # authentication verify success.
+    if validate is True:
+        return jsonify({'username': username, 'validation': 'True'})
+    # authentication verify failed.
+    return jsonify({'username': username, 'validation': 'False'})
+
+
 # bad requests holder
-def bad_request():
-    pass
+def bad_request(message):
+    return error_response(400, message)
+
+
+# error response
+def error_response(status_code, message=None):
+    payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
+    if message:
+        payload['message'] = message
+    response = jsonify(payload)
+    response.status_code = status_code
+    return response
--- basic_tests.py
+++ basic_tests.py
@@ -3,18 +3,21 @@ from app.models import User
 
 if __name__ == '__main__':
 
-    # u = User(
-    #     username='Mike',
-    #     password_hash='0a',
-    #     email='Mike@example.com',
-    #     phone_number='11311311113',
-    # )
-    #
-    # db.session.add(u)
-    # db.session.commit()
-    #
-    # users = User.query.all()
-    # print(users)
-    username = "Mike"
-    users = User.query.filter(User.username == username).all()
+    u = User(
+        username='Mike',
+        email='Mike@example.com',
+        phone_number='11311311113',
+    )
+
+    u.set_password('123456')
+
+    print(u.check_password('123456'))
+    db.session.add(u)
+    db.session.commit()
+
+    users = User.query.all()
     print(users)
+    # username = "Mike"
+    # users = User.query.filter(User.username == username).all()
+    # print(users)
+    pass
